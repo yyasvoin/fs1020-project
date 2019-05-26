f7827bc6cfb8f3c791abc4c2d584f8a49786d268
yyasvoin <yasvoin@gmail.com>
2019-05-26 13:28:35 -0400

    update

--- router/index.js
+++ router/index.js
@@ -10,5 +10,33 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
+router.post('/register', (req, res, next) => {
+  res.sendStatus(200);
+});
+
+router.post('/login', (req, res, next) => {
+  res.sendStatus(200);
+});
+
+router.get('/product', (req, res, next) => {
+  res.sendStatus(200);
+});
+
+router.get('/item/:id', (req, res, next) => {
+  res.sendStatus(200);
+});
+
+router.post('/item', (req, res, next) => {
+  res.sendStatus(200);
+});
+
+router.delete('/item/:id', (req, res, next) => {
+  res.sendStatus(200);
+});
+
+router.patch('/item/:id', (req, res, next) => {
+  res.sendStatus(200);
+});
+
 
 module.exports = router;
