3bfceec89bc6025dd01f4d55e08c16b94d688dd3
Yuri Yasvoin <yasvoin@gmail.com>
2019-05-26 16:50:00 +0000

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,32 +1,44 @@
 'use strict';
 
 const express = require('express');
-const router = require('./router');
-const defaultErrorHandler = require('./middleware/default-error-handler');
+
+const router = express.Router();
 
 
-// Create an instance of an Express server app
-const app = express();
-
-// Use the EJS templating engine (comment this out if no webpages are generated)
-app.set('view engine', 'ejs');
-
-// Serve static content, URL paths must start with "/static"
-app.use('/static', express.static('static'));
-
-// Parse incoming JSON
-app.use(express.json({ extended: true }));
-
-// Add our defined routes from router.js
-app.use(router);
-
-// Default error handler should in any of our routes we call next() with an error
-app.use(defaultErrorHandler);
-
-
-// Start the express server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Express server started on port ${port}.`);
+// Renders the home page
+router.get('/', (req, res) => {
+  res.render('home');
 });
+
+router.post('/register', (req, res, next) => {
+  res.statusCode = 201;
+});
+
+router.post('/login', (req, res, next) => {
+  res.statusCode = 202;
+});
+
+router.get('/product', (req, res, next) => {
+  res.statusCode = 200;
+});
+
+router.get('/item/:id', (req, res, next) => {
+  res.statusCode = 200;
+});
+
+router.post('/item', (req, res, next) => {
+  res.statusCode = 200;
+});
+
+router.delete('/item/:id', (req, res, next) => {
+  res.statusCode = 200;
+});
+
+router.patch('/item/:id', (req, res, next) => {
+  res.statusCode = 200;
+});
+
+
+module.exports = router;
+
 
